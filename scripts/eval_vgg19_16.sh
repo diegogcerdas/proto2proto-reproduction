72b4c9809bb80458cf481f97d645d709bc3a88f9
Diego Garcia Cerdas <diego.gcerdas@gmail.com>
2023-01-26 23:09:50 +0100

    Eval files for vgg16

---
+++ scripts/eval_vgg19_16.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+nonRandom=org
+runName=eval_setting
+newRun=false
+serviceType=evaluate
+ablationType=VGG19_16_cub
+
+mkdir Experiments/$ablationType/$runName/$nonRandom
+expOut=Experiments/$ablationType/$runName/$nonRandom/$nonRandom.out
+errorOut=Experiments/$ablationType/$runName/$nonRandom/error+$nonRandom.out
+
+cp Experiments/$ablationType/$runName/args.yaml Experiments/$ablationType/$runName/$nonRandom/args.yaml
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -u main.py $runName $newRun $serviceType $nonRandom $ablationType > $expOut 2>$errorOut
