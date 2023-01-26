c3429e6d7bdc9a8e29736179db6fe6d99180499a
Diego Garcia Cerdas <diego.gcerdas@gmail.com>
2023-01-26 13:56:32 +0100

    Changes to train VGG16

---
+++ scripts/train_baseline_cub_vgg16.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+nonRandom=org
+runName=student_VGG16
+newRun=false
+serviceType=recognition
+ablationType=VGG19_16_cub
+
+mkdir Experiments/$ablationType/$runName/$nonRandom
+expOut=Experiments/$ablationType/$runName/$nonRandom/$nonRandom.out
+errorOut=Experiments/$ablationType/$runName/$nonRandom/error+$nonRandom.out
+
+cp Experiments/$ablationType/$runName/args.yaml Experiments/$ablationType/$runName/$nonRandom/args.yaml
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -u main.py $runName $newRun $serviceType $nonRandom $ablationType > $expOut 2>$errorOut
