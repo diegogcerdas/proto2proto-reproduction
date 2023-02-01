5d6c2e073ccc35b08be993c24d66d85daf41c824
Diego Garcia Cerdas <lcur0855@test3.irc.sara.nl.irc.sara.nl>
2023-02-01 21:02:35 +0100

    minor change

--- example.py
+++ example.py
@@ -11,9 +11,9 @@ def main():
 
     # VGG19 -> VGG 11
     teacher = init_model('arguments/vgg19_teacher.yaml', device)
-    baseline_student = init_model('arguments/vgg11_baseline.yaml', device)
-    kd_student = init_model('arguments/vgg11_kd.yaml', device)
-    run_experiment('vgg19_11', teacher, baseline_student, kd_student)
+    #baseline_student = init_model('arguments/vgg11_baseline.yaml', device)
+    #kd_student = init_model('arguments/vgg11_kd.yaml', device)
+    #run_experiment('vgg19_11', teacher, baseline_student, kd_student)
 
     # VGG19 -> VGG 16
     baseline_student = init_model('arguments/vgg16_baseline.yaml', device)
@@ -29,7 +29,7 @@ def init_model(args_filename, device):
 
 def run_experiment(experiment_name, teacher, baseline_student, kd_student):
     print(f'Running experiment: {experiment_name}')
-    results_dir = os.path.join('results', experiment_name, str(dist_threshold))
+    results_dir = os.path.join('results', experiment_name)
     makedir(results_dir)
     
     teacher_accuracy = teacher.compute_accuracy()
