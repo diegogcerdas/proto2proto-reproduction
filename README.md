9dbd38d5d0d90b8733fa3d525ece3c79481b5c37
Diego Garcia Cerdas <diego.gcerdas@gmail.com>
2023-02-03 21:33:01 +0100

    First step for documentation

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-TO-DO: README
+# On the reproducibility of ”Proto2Proto: Can you recognize the car, the way I do?”
+
+This repo concerns a reproducibility study of the paper: [_Proto2Proto: Can You Recognize the Car, the Way I Do?_ ](https://openaccess.thecvf.com/content/CVPR2022/html/Keswani_Proto2Proto_Can_You_Recognize_the_Car_the_Way_I_Do_CVPR_2022_paper.html). It builds upon the authors' [original implementation](https://github.com/archmaester/proto2proto), and provides wrapping code to run evaluation in an easy and efficient manner.
+
+## Creating Conda Environment
+    conda env create -f environment.yml python=3.6
+    conda activate proto2proto
+
+## Setting Up Dataset and Required Code
+Running the following command ensures that the CUB-200-2011 dataset is downloaded into `./datasets/CUB_200_2011/`, and the required code (a folder called `lib`) is cloned into `./reproduction/` setup:
+
+    bash setup_reproduction.sh
+
+Please make sure to run the above command before running other scripts or notebooks.
+
+## Model Weights and Results from our Reproducibility Study
+
+We provide the model weights of the networks used in our study through [this anonymous storage](https://drive.google.com/drive/folders/1ZgEKQe9tX6loGBip4TQ1HIRK45SqWMBd?usp=sharing), by downloading `checkpoints.zip`. You can also download `results.zip`, containing the metrics for our experiments and prototype matches between teacher and student, and `nearest.zip` containing the nearest training patches for each model's prototypes.
+
+- Please unzip these files into the current directory as `./checkpoints/`, `./results/`, and `./nearest/` respectively.
+
+### Example notebook
+
+To reproduce the figures from our study, we provide a Jupyter notebook `example.ipynb`. This notebook further explains the structure of the folders setup above.
+
+## Running Evaluation from Scratch
+
+If you wish to perform evaluation from scratch, please run:
+
+    # For interpretability metrics, accuracy, and prototype-matching
+    python evaluation.py  
+
+    # For finding nearest training patches for each model's prototypes
+    python find_nearest.py  
+
+ - To perform evaluation on additional ProtoPNet models, simply modify the `main` method in these files to point to your models.
+
+## Aknowledgement
+
+Our code is built on top of [Proto2Proto](https://github.com/archmaester/proto2proto) and [ProtoPNet](https://github.com/cfchen-duke/ProtoPNet).
--- evaluation.py
+++ evaluation.py
@@ -16,6 +16,7 @@ def main():
     run_experiment('vgg19_11', teacher, baseline_student, kd_student)
 
     # VGG19 -> VGG 16
+    teacher = init_model('arguments/vgg19_teacher.yaml', device)
     baseline_student = init_model('arguments/vgg16_baseline.yaml', device)
     kd_student = init_model('arguments/vgg16_kd.yaml', device)
     run_experiment('vgg19_16', teacher, baseline_student, kd_student)
@@ -35,40 +36,40 @@ def run_experiment(experiment_name, teacher, baseline_student, kd_student):
 
     save_indices_scores(results_dir, teacher, baseline_student, kd_student)
     
-    # teacher_accuracy = teacher.compute_accuracy()
-    # baseline_accuracy = baseline_student.compute_accuracy()
-    # kd_accuracy = kd_student.compute_accuracy()
+    teacher_accuracy = teacher.compute_accuracy()
+    baseline_accuracy = baseline_student.compute_accuracy()
+    kd_accuracy = kd_student.compute_accuracy()
     
-    # baseline_pms, baseline_best_allocation = baseline_student.compute_pms(teacher.indices_scores)
-    # np.save(os.path.join(results_dir, 'baseline_best_allocation.npy'), baseline_best_allocation)
-    # kd_pms, kd_best_allocation = kd_student.compute_pms(teacher.indices_scores)
-    # np.save(os.path.join(results_dir, 'kd_best_allocation.npy'), kd_best_allocation)
+    baseline_pms, baseline_best_allocation = baseline_student.compute_pms(teacher.indices_scores)
+    np.save(os.path.join(results_dir, 'baseline_best_allocation.npy'), baseline_best_allocation)
+    kd_pms, kd_best_allocation = kd_student.compute_pms(teacher.indices_scores)
+    np.save(os.path.join(results_dir, 'kd_best_allocation.npy'), kd_best_allocation)
 
-    # for dist_threshold in [0.01, 0.1, 0.2, 0.45, 1.0, 3.0, 5.0, None]:
-    #     results = {
-    #         'aap': {
-    #             'teacher': teacher.compute_aap(dist_threshold),
-    #             'baseline_student': baseline_student.compute_aap(dist_threshold),
-    #             'kd_student': kd_student.compute_aap(dist_threshold)
-    #         },
-    #         'ajs': {
-    #             'baseline_student': baseline_student.compute_ajs(dist_threshold, teacher.indices_scores),
-    #             'kd_student': kd_student.compute_ajs(dist_threshold, teacher.indices_scores)
-    #         },
-    #         'accuracy': {
-    #             'teacher': teacher_accuracy,
-    #             'baseline_student': baseline_accuracy,
-    #             'kd_student': kd_accuracy
-    #         },
-    #         'pms': {
-    #             'baseline_student': baseline_pms,
-    #             'kd_student': kd_pms
-    #         }
-    #     }
-    #     results_dist_dir = os.path.join(results_dir, str(dist_threshold))
-    #     makedir(results_dist_dir)
-    #     with open(os.path.join(results_dist_dir, 'metrics.yaml'), 'w') as file:
-    #         yaml.dump(results, file)
+    for dist_threshold in [0.01, 0.1, 0.2, 0.45, 1.0, 3.0, 5.0, None]:
+        results = {
+            'aap': {
+                'teacher': teacher.compute_aap(dist_threshold),
+                'baseline_student': baseline_student.compute_aap(dist_threshold),
+                'kd_student': kd_student.compute_aap(dist_threshold)
+            },
+            'ajs': {
+                'baseline_student': baseline_student.compute_ajs(dist_threshold, teacher.indices_scores),
+                'kd_student': kd_student.compute_ajs(dist_threshold, teacher.indices_scores)
+            },
+            'accuracy': {
+                'teacher': teacher_accuracy,
+                'baseline_student': baseline_accuracy,
+                'kd_student': kd_accuracy
+            },
+            'pms': {
+                'baseline_student': baseline_pms,
+                'kd_student': kd_pms
+            }
+        }
+        results_dist_dir = os.path.join(results_dir, str(dist_threshold))
+        makedir(results_dist_dir)
+        with open(os.path.join(results_dist_dir, 'metrics.yaml'), 'w') as file:
+            yaml.dump(results, file)
 
     print(f'Finished experiment: {experiment_name}')
 
